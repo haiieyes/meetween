6c48c20919e87c6a84b50705a32c88157ed9ab92
Ubuntu <ubuntu@ip-172-31-28-50.ec2.internal>
2019-09-26 09:20:27 +0000

    Initial commit with Google Maps up and running.

---
+++ script.js
@@ -0,0 +1,11 @@
+/* global google */
+/* global $ */
+
+let map;
+
+$(function() {
+map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: -34.397, lng: 150.644},
+  zoom: 8
+});
+});
